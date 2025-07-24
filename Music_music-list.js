6545dccdee039362592a7d5435067c08603b522b
DangVanQuy.Blogspot.com <68425960+kinhlangnghiem@users.noreply.github.com>
2025-07-24 19:37:31 +0700

    Update Music_music-list.js

--- Music_music-list.js
+++ Music_music-list.js
@@ -3,45 +3,45 @@
 //   {
 //     name: "Here is the music name",
 //     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in  formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
 //   }
 
 let allMusic = [
   {
     name: "Ấn Quang ĐẠI SƯ Hộ Quốc Tiêu Đọc Giọng Nữ.mp3",
     artist: "Đại Sư Ấn Quang",
-    img: "https://i.ex-cdn.com/phatgiao.org.vn/files/content/2023/09/30/00-1847.jpg",
-    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20%C4%90%E1%BA%A0I%20S%C6%B0%20H%E1%BB%99%20Qu%E1%BB%91c%20Ti%C3%AAu%20%C4%90%E1%BB%8Dc%20Gi%E1%BB%8Dng%20N%E1%BB%AF.mp3",
+    img: "https://i.ex-cdn.com/phatgiao.org.vn/files/content/2023/09/30/00-1847",
+    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20%C4%90%E1%BA%A0I%20S%C6%B0%20H%E1%BB%99%20Qu%E1%BB%91c%20Ti%C3%AAu%20%C4%90%E1%BB%8Dc%20Gi%E1%BB%8Dng%20N%E1%BB%AF",
   },
   {
     name: "Đại Sư Ấn Quang Lá Thơ Tịnh Độ",
     artist: "Đại Sư Ấn Quang",
-    img: "https://i1.sndcdn.com/artworks-A5y1Qheqy0jgQET7-FrfcMQ-t500x500.jpg",
-    src: "https://archive.org/download/LATHUTINHDO_ANQUANGDAISU/L%C3%A1%20Th%C6%B0%20T%E1%BB%8Bnh%20%C4%90%E1%BB%99%201%20%26%202.mp3",
+    img: "https://i1.sndcdn.com/artworks-A5y1Qheqy0jgQET7-FrfcMQ-t500x500",
+    src: "https://archive.org/download/LATHUTINHDO_ANQUANGDAISU/L%C3%A1%20Th%C6%B0%20T%E1%BB%8Bnh%20%C4%90%E1%BB%99%201%20%26%202",
   },
   {
     name: "Hoà Thượng Thích thiền Tâm Lá Thơ Tịnh Độ",
     artist: "Đại Sư Ấn Quang",
-    img: "https://i.ytimg.com/vi/zB0QvlK7npk/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGH8gVyglMA8=&rs=AOn4CLDqQEnm94yt1Rmb_LZ5PkLj-XJYOA",
-    src: "https://archive.org/download/LA_THO_TINH_DO/Th%C3%ADch%20Thi%E1%BB%81n%20T%C3%A2m%20-%20L%C3%A1%20Th%C6%A1%20T%E1%BB%8Bnh%20%C4%90%E1%BB%99.mp3",
+    img: "https://wenshuchan-online.weebly.com/uploads/9/4/8/2/9482304/5033758_orig",
+    src: "https://archive.org/download/LA_THO_TINH_DO/Th%C3%ADch%20Thi%E1%BB%81n%20T%C3%A2m%20-%20L%C3%A1%20Th%C6%A1%20T%E1%BB%8Bnh%20%C4%90%E1%BB%99",
   },
   {
     name: "Đại Sư Ấn Quang Hộ Quốc Tiêu Tai",
     artist: "Mike Archangelo",
-    img: "https://upload.wikimedia.org/wikipedia/commons/7/77/Yinguang.jpg",
-    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4N%20QUANG%20%C4%90%E1%BA%A0I%20S%C6%AF%20H%E1%BB%98%20QU%E1%BB%90C%20TI%C3%8AU%20TAI.mp3",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/77/Yinguang",
+    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4N%20QUANG%20%C4%90%E1%BA%A0I%20S%C6%AF%20H%E1%BB%98%20QU%E1%BB%90C%20TI%C3%8AU%20TAI",
   },
   {
     name: "ẤN QUANG VĂN SAO TẬP 01",
     artist: "Jim Yosef x Roy",
-    img: "https://wenshuchan-online.weebly.com/uploads/9/4/8/2/9482304/5033758_orig.jpg",
-    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20Ph%C3%A1p%20S%C6%B0%20V%C4%83n%20Sao%20T%E1%BA%ADp%2001.mp3",
+    img: "https://wenshuchan-online.weebly.com/uploads/9/4/8/2/9482304/5033758_orig",
+    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20Ph%C3%A1p%20S%C6%B0%20V%C4%83n%20Sao%20T%E1%BA%ADp%2001",
   },
   {
     name: "ẤN QUANG VĂN SAO TẬP 02",
     artist: "NCS Release",
-    img: "https://www.tsemrinpoche.com/wp-content/uploads/2018/03/10Chinese002.jpg",
-    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20Ph%C3%A1p%20S%C6%B0%20V%C4%83n%20Sao%20T%E1%BA%ADp%2002.mp3",
+    img: "https://www.tsemrinpoche.com/wp-content/uploads/2018/03/10Chinese002",
+    src: "https://archive.org/download/AnQuangVanSAo/%E1%BA%A4n%20Quang%20Ph%C3%A1p%20S%C6%B0%20V%C4%83n%20Sao%20T%E1%BA%ADp%2002",
   },
 ];
