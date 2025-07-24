f0e747ddb6e20fb41ad3b2f14a143e71d04640e1
DangVanQuy.Blogspot.com <68425960+kinhlangnghiem@users.noreply.github.com>
2025-07-24 18:43:37 +0700

    Update Music_music-list.js

--- Music_music-list.js
+++ Music_music-list.js
@@ -3,8 +3,8 @@
 //   {
 //     name: "Here is the music name",
 //     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//     img: "image name here - remember img must be in  formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
 //   }
 
 let allMusic = [
